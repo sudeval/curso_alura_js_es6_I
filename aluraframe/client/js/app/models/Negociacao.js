9f67b1186cdf90afe0c13b2e7102c860c462d02b
Tiago Sudeval Ferreira Duarte <sudeval@gmail.com>
2018-10-17 00:23:35 -0300

    concluindo exercicios com encapsulamento, get e objetos imutaveis

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,12 +1,27 @@
 class Negociacao {
 
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        // _ (underline) é uma convenção para não acessar a propriedade
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+        // tornando a instância imutavel
+        Object.freeze(this);
     }
 
-    obtemVolume() {
+    get volume() {
         return this.quantidade * this.valor
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get data() {
+        return this.data;
     }
 }
